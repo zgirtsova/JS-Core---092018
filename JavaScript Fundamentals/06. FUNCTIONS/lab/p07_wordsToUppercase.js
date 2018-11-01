9e27aecfbeea2fb6c768ec6f93d40c84b80d47ab
Midori_san <zgirtsova@gmail.com>
2018-11-01 22:43:09 +0200

    Functions 06

--- JavaScript Fundamentals/06. FUNCTIONS/lab/p07_wordsToUppercase.js
+++ JavaScript Fundamentals/06. FUNCTIONS/lab/p07_wordsToUppercase.js
@@ -1,11 +1,12 @@
 function toUpperCase(input) {
     input = input.toUpperCase();
     
-    let split = input.split(/\W+/);
+    let split = input.split(/\W+/).filter(e => e !== '').join(', ');
 
-    split = split.filter(e => e != '');
+    //split = split.filter(e => e !== '');
 
-    console.log(split.join(', '));
+    //console.log(split.join(', '));
+    console.log(split);
 }
 
 toUpperCase('Hi, how are you?');
